29d9a69b73fe4804699ffa2560af20c0848d194b
LLeo Harress <13482930+lharress@users.noreply.github.com>
2021-08-23 12:27:17 +0100

    Tags / React - Fix missing prop bug

--- tags/react/src/components/MentionMe.js
+++ tags/react/src/components/MentionMe.js
@@ -3,11 +3,29 @@ import { useEffect, memo } from "react";
 const ENDPOINT =
   "https://tag-demo.mention-me.com/api/v2/referreroffer/mmbd3a737d";
 
-const MentionMe = ({ implementation, situation, locale }) => {
+const addTerm = (url, urlParams) => {
+    const anchor = document.createElement("a");
+    anchor.href = url;
+
+    Object.entries(urlParams).filter(([_key, value]) => {
+      return !!value;
+    }).forEach(([key, value]) => {
+        anchor.search += `${anchor.search ? "&" : ""}${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
+    });
+
+    return anchor.href;
+};
+
+const MentionMe = ({ implementation = "", situation = "", locale = "en_GB" }) => {
   useEffect(() => {
     // Create & Insert MM Script
     const script = document.createElement("script");
-    script.src = ENDPOINT + `?situation=${situation}&implementation=${encodeURIComponent(implementation)}&locale=${encodeURIComponent(locale)}`;
+    script.src = addTerm(ENDPOINT, {
+      'situation': situation,
+      'locale': locale,
+      'implementation': implementation,
+    });
+
     const head = document.querySelector("head");
     head.appendChild(script);
 
